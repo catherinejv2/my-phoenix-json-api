7b7c0337c2742e92eccb49ed80f81220769808cc
Catherine Armstrong <c.armstrong@sportradar.com>
2021-01-25 16:38:30 -0600

    added doc

--- lib/my_app/account/user.ex
+++ lib/my_app/account/user.ex
@@ -1,4 +1,6 @@
 defmodule MyApp.Account.User do
+  @moduledoc false
+
   use Ecto.Schema
   import Ecto.Changeset
 
--- mix.exs
+++ mix.exs
@@ -1,4 +1,6 @@
 defmodule MyApp.MixProject do
+  @moduledoc false
+
   use Mix.Project
 
   def project do
@@ -43,7 +45,9 @@ defmodule MyApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 
